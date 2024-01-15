63b3d814e606bb2c5a9421030ce4637a245b83d3
Mitchell Goff <mitchellgoffpc@gmail.com>
2024-01-15 01:57:36 -0800

    Save data from arm cam in play.py

--- chess/play.py
+++ chess/play.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 import io
+import time
 import chess
 import chess.pgn
 import chess.svg
 import chess.engine
 import tkinter as tk
+import pygame
+import pygame.camera
 from pathlib import Path
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+
+WEBCAM_NAME = 'Logitech Webcam C930e'
+PHOTO_ANGLES = [
+  [64, 31, 117, 90, -70, -116],
+  [-67, 20, 112, 90, 66, -66],
+]
 
 def read_pgns(pgn_fn):
   game = []
@@ -21,11 +30,16 @@ def read_pgns(pgn_fn):
 
 
 class ChessGui(tk.Frame):
-  def __init__(self, parent, pgns, *args, **kwargs):
+  def __init__(self, parent, pgns, arm, cam, data_dir, *args, **kwargs):
     tk.Frame.__init__(self, parent, *args, **kwargs)
     self.parent = parent
     self.pgns = pgns
+    self.arm = arm
+    self.cam = cam
+    self.data_dir = data_dir
+
     self.grid()
+    self.game, self.move = 0, 0
     self.chessboard = next(pgns)
     self.canvas = tk.Canvas(self, width=510, height=510, bg="grey")
     self.canvas.grid(row=0, column=0)
@@ -44,14 +58,42 @@ class ChessGui(tk.Frame):
 
   def click(self, event):
     self.chessboard = self.chessboard.next()
+    self.move += 1
     if self.chessboard is None:
       self.chessboard = next(pgns)
+      self.game += 1
+      self.move = 0
+
     self.draw_board()
+    self.parent.after(100, self.take_snapshot)
+
+  def take_snapshot(self):
+    for i, angles in enumerate(PHOTO_ANGLES):
+      self.arm.set_servo_angle(servo_id=None, angle=angles, speed=50, mvacc=90, is_radian=False, wait=True)
+      time.sleep(0.2)
+      image = cam.get_image()
+      pygame.image.save(image, data_dir / f'positions/{self.game:03d}-{self.move:03d}-{i}.jpg')
 
 
 if __name__ == "__main__":
-  pgns = read_pgns(Path(__file__).parent / 'data/2022.pgn')
-  root = tk.Tk()
-  ChessGui(root, pgns)
-  root.mainloop()
+  from xarm.wrapper import XArmAPI
 
+  arm = XArmAPI('192.168.1.170')
+  arm.set_mode(0)
+  arm.set_state(0)
+  time.sleep(0.1)
+
+  pygame.camera.init()
+  camlist = pygame.camera.list_cameras()
+  if WEBCAM_NAME not in camlist:
+    raise RuntimeError("No camera on current device")
+  cam = pygame.camera.Camera(WEBCAM_NAME, (640, 480))
+  cam.start()
+
+  data_dir = Path(__file__).parent / 'data'
+  (data_dir / 'positions').mkdir(exist_ok=True)
+
+  pgns = read_pgns(data_dir / '2022.pgn')
+  root = tk.Tk()
+  ChessGui(root, pgns, arm, cam, data_dir)
+  root.mainloop()
--- test_arm.py
+++ test_arm.py
@@ -1,8 +1,19 @@
 import time
+import numpy as np
 from xarm.wrapper import XArmAPI
-arm = XArmAPI('192.168.1.170')
 
-while True:
-  _, pos = arm.get_position()
-  print(pos)
-  time.sleep(0.2)
+arm = XArmAPI('192.168.1.170')
+arm.set_mode(0)
+arm.set_state(0)
+time.sleep(1)
+
+arm.set_servo_angle(servo_id=None, angle=[64, 31, 117, 90, -70, -116], speed=40, is_radian=False, wait=True)
+time.sleep(3)
+
+arm.set_servo_angle(servo_id=None, angle=[-67, 20, 112, 90, 66, -66], speed=40, is_radian=False, wait=True)
+time.sleep(3)
+
+# while True:
+#   _, pos = arm.get_position()
+#   print(pos)
+#   time.sleep(0.2)
