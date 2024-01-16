6ff9e1872f16ba287b2b3accee8c967c07fedd51
Mitchell Goff <mitchellgoffpc@gmail.com>
2024-01-15 17:49:32 -0800

    chess/play.py ready for data generation

--- chess/play.py
+++ chess/play.py
@@ -6,16 +6,18 @@ import chess.pgn
 import chess.svg
 import chess.engine
 import tkinter as tk
+import threading
 import pygame
 import pygame.camera
 from pathlib import Path
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
 
+FILL_COLOR = '#cc0000cc'
 WEBCAM_NAME = 'Logitech Webcam C930e'
 PHOTO_ANGLES = [
-  [64, 31, 117, 90, -70, -116],
-  [-67, 20, 112, 90, 66, -66],
+  [43.30, 34.50, 121.60, 92.00, -43.20, 234.20],
+  [-55.72, 41.4, 123.13, 275.02, -58.07, 105.51],
 ]
 
 def read_pgns(pgn_fn):
@@ -37,18 +39,36 @@ class ChessGui(tk.Frame):
     self.arm = arm
     self.cam = cam
     self.data_dir = data_dir
+    self.game_id, self.move_id = 0, 0
+    self.hand_photo_id = -1
+    self.chessboard = next(self.pgns)
+
+    existing_games = set(int(x.name.split('-')[0]) for x in (self.data_dir / 'positions').iterdir())
+    while self.game_id in existing_games:
+      self.next_move()
 
     self.grid()
-    self.game, self.move = 0, 0
-    self.chessboard = next(pgns)
     self.canvas = tk.Canvas(self, width=510, height=510, bg="grey")
     self.canvas.grid(row=0, column=0)
     self.canvas.bind("<Button-1>", self.click)
     self.selected_position = None
     self.draw_board()
 
+    self.hand_snapshot_thread = threading.Thread(target=self.take_hand_snapshots, daemon=True)
+    self.hand_snapshot_thread.start()
+
+  def next_move(self):
+    self.chessboard = self.chessboard.next()
+    self.move_id += 1
+    if self.chessboard is None:
+      self.chessboard = next(self.pgns)
+      self.game_id += 1
+      self.move_id = 0
+
   def draw_board(self):
-    board_svg = chess.svg.board(board=self.chessboard.board(), fill={self.selected_position: '#cc0000cc'}, size=510)
+    move = self.chessboard.move
+    fill = {move.from_square: FILL_COLOR, move.to_square: FILL_COLOR} if move else {}
+    board_svg = chess.svg.board(board=self.chessboard.board(), fill=fill, size=510)
     drawing = svg2rlg(io.StringIO(board_svg))
     imgdata = io.BytesIO()
     renderPM.drawToFile(drawing, imgdata, fmt='png')
@@ -57,22 +77,27 @@ class ChessGui(tk.Frame):
     self.canvas.create_image(0, 0, anchor='nw', image=self.board_photo)
 
   def click(self, event):
-    self.chessboard = self.chessboard.next()
-    self.move += 1
-    if self.chessboard is None:
-      self.chessboard = next(pgns)
-      self.game += 1
-      self.move = 0
+    self.hand_photo_id = -1
+    self.take_move_snapshots()
+    self.hand_photo_id = 0
 
+    self.next_move()
     self.draw_board()
-    self.parent.after(100, self.take_snapshot)
 
-  def take_snapshot(self):
+  def take_move_snapshots(self):
     for i, angles in enumerate(PHOTO_ANGLES):
-      self.arm.set_servo_angle(servo_id=None, angle=angles, speed=50, mvacc=90, is_radian=False, wait=True)
+      self.arm.set_servo_angle(servo_id=None, angle=angles, speed=100, mvacc=180, is_radian=False, wait=True)
       time.sleep(0.2)
       image = cam.get_image()
-      pygame.image.save(image, data_dir / f'positions/{self.game:03d}-{self.move:03d}-{i}.jpg')
+      pygame.image.save(image, data_dir / f'positions/{self.game_id:03d}-{self.move_id:03d}-{i}.jpg')
+
+  def take_hand_snapshots(self):
+    while True:
+      image = cam.get_image()
+      if self.hand_photo_id != -1:
+        pygame.image.save(image, data_dir / f'hands/{self.game_id:03d}-{self.move_id-1:03d}-{self.hand_photo_id}.jpg')
+        self.hand_photo_id += 1
+      time.sleep(0.5)
 
 
 if __name__ == "__main__":
@@ -92,6 +117,7 @@ if __name__ == "__main__":
 
   data_dir = Path(__file__).parent / 'data'
   (data_dir / 'positions').mkdir(exist_ok=True)
+  (data_dir / 'hands').mkdir(exist_ok=True)
 
   pgns = read_pgns(data_dir / '2022.pgn')
   root = tk.Tk()
--- test_arm.py
+++ test_arm.py
@@ -7,11 +7,22 @@ arm.set_mode(0)
 arm.set_state(0)
 time.sleep(1)
 
-arm.set_servo_angle(servo_id=None, angle=[64, 31, 117, 90, -70, -116], speed=40, is_radian=False, wait=True)
+arm.set_servo_angle(servo_id=None, angle=[43.30, 34.50, 121.60, 92.00, -43.20, 234.20], speed=100, mvacc=180, is_radian=False, wait=True)
 time.sleep(3)
+arm.set_servo_angle(servo_id=None, angle=[-55.72, 41.4, 123.13, 275.02, -58.07, 105.51], speed=100, mvacc=180, is_radian=False, wait=True)
 
-arm.set_servo_angle(servo_id=None, angle=[-67, 20, 112, 90, 66, -66], speed=40, is_radian=False, wait=True)
-time.sleep(3)
+_, pos = arm.get_servo_angle(servo_id=None)
+print(pos)
+
+# arm.set_mode(0)
+# arm.set_state(0)
+# time.sleep(1)
+
+# arm.set_servo_angle(servo_id=None, angle=[64, 31, 117, 90, -70, -116], speed=40, is_radian=False, wait=True)
+# time.sleep(3)
+#
+# arm.set_servo_angle(servo_id=None, angle=[-67, 20, 112, 90, 66, -66], speed=40, is_radian=False, wait=True)
+# time.sleep(3)
 
 # while True:
 #   _, pos = arm.get_position()
